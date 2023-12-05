45ecdd940358dba2704d2e646a891ef8ee974501
anandanur <130210181+anandanur@users.noreply.github.com>
2023-12-05 02:32:15 +0000

    update

--- app.py
+++ app.py
@@ -1,71 +1,75 @@
 import streamlit as st
 from sqlalchemy import text
 
-list_doctor = ['', 'dr. Nurita', 'dr. Yogi', 'dr. Wibowo', 'dr. Ulama', 'dr. Ping']
-list_symptom = ['', 'male', 'female']
 
 conn = st.connection("postgresql", type="sql", 
                      url="postgresql://anandanurd13:liuoJI0dh8CQ@ep-bitter-credit-99134373.us-east-2.aws.neon.tech/fp3")
 with conn.session as session:
-    query = text('CREATE TABLE IF NOT EXISTS SCHEDULE (id serial, doctor_name varchar, patient_name varchar, gender char(25), \
-                                                       symptom text, handphone varchar, address text, tanggal date);')
+    query = text('CREATE TABLE IF NOT EXISTS orders (id serial, customer_name text, email text, phone_number text, \
+                  address text, product_name text, quantity int, order_date date, order_status text, \
+                  tracking_number text, estimated_arrival_date date);')
     session.execute(query)
 
-st.header('SIMPLE HOSPITAL DATA MANAGEMENT SYS')
+st.header('SIMPLE SHOP DATA MANAGEMENT')
 page = st.sidebar.selectbox("Pilih Menu", ["View Data","Edit Data"])
 
 if page == "View Data":
-    data = conn.query('SELECT * FROM schedule ORDER By id;', ttl="0").set_index('id')
+    data = conn.query('SELECT * FROM orders ORDER By id;', ttl="0").set_index('id')
     st.dataframe(data)
 
 if page == "Edit Data":
     if st.button('Tambah Data'):
         with conn.session as session:
-            query = text('INSERT INTO schedule (doctor_name, patient_name, gender, symptom, handphone, address, waktu, tanggal) \
-                          VALUES (:1, :2, :3, :4, :5, :6, :7, :8);')
-            session.execute(query, {'1':'', '2':'', '3':'', '4':'[]', '5':'', '6':'', '7':None, '8':None})
+            query = text('INSERT INTO ORDERS (customer_name, email, phone_number, address, product_name,order_status) \
+                          VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11);')
+            session.execute(query, {'1':'', '2':'', '3':'', '4':'', '5':'', '6':'', '7':'', '8':None, '9':'', '10':'', '11':None})
             session.commit()
 
-    data = conn.query('SELECT * FROM schedule ORDER By id;', ttl="0")
+    data = conn.query('SELECT * FROM orders ORDER By id;', ttl="0")
     for _, result in data.iterrows():        
         id = result['id']
-        doctor_name_lama = result["doctor_name"]
-        patient_name_lama = result["patient_name"]
-        gender_lama = result["gender"]
-        symptom_lama = result["symptom"]
-        handphone_lama = result["handphone"]
+        customer_name_lama = result["customer_name"]
+        email_lama = result["email"]
+        phone_number_lama = result["phone_number"]
         address_lama = result["address"]
-        waktu_lama = result["waktu"]
-        tanggal_lama = result["tanggal"]
+        product_name_lama = result["product_name"]
+        quantity_lama = result["quantity"]
+        order_date_lama = result["order_date"]
+        order_status_lama = result["order_status"]
+        tracking_number_lama= result ["tracking_number"]
+        estimated_arrival_date_lama = result ["estimated_arrival_date"]
 
-        with st.expander(f'a.n. {patient_name_lama}'):
+        with st.expander(f'a.n. {customer_name_lama}'):
             with st.form(f'data-{id}'):
-                doctor_name_baru = st.selectbox("doctor_name", list_doctor, list_doctor.index(doctor_name_lama))
-                patient_name_baru = st.text_input("patient_name", patient_name_lama)
-                gender_baru = st.selectbox("gender", list_symptom, list_symptom.index(gender_lama))
-                symptom_baru = st.multiselect("symptom", ['cough', 'flu', 'headache', 'stomache'], eval(symptom_lama))
-                handphone_baru = st.text_input("handphone", handphone_lama)
+                customer_name_baru = st.text_input("customer_name",customer_name_lama)
+                email_baru = st.text_input("email", email_lama)
+                phone_number_baru = st.text_input("phone_number", phone_number_lama)
                 address_baru = st.text_input("address", address_lama)
-                waktu_baru = st.time_input("waktu", waktu_lama)
-                tanggal_baru = st.date_input("tanggal", tanggal_lama)
-                
+                product_name_baru = st.text_input("product_name", product_name_lama)
+                quantity_baru = st.text_input("quantity", quantity_lama)
+                order_date_baru = st.date_input("order_date", order_date_lama)
+                order_status_baru = st.text_input("order_status", order_status_lama)
+                tracking_number_baru = st.text_input("tracking_number", tracking_number_lama)
+                estimated_arrival_date_baru = st.date_input ("estimated_arrival_date", estimated_arrival_date_lama)
                 col1, col2 = st.columns([1, 6])
 
                 with col1:
                     if st.form_submit_button('UPDATE'):
                         with conn.session as session:
-                            query = text('UPDATE schedule \
-                                          SET doctor_name=:1, patient_name=:2, gender=:3, symptom=:4, \
-                                          handphone=:5, address=:6, waktu=:7, tanggal=:8 \
-                                          WHERE id=:9;')
-                            session.execute(query, {'1':doctor_name_baru, '2':patient_name_baru, '3':gender_baru, '4':str(symptom_baru), 
-                                                    '5':handphone_baru, '6':address_baru, '7':waktu_baru, '8':tanggal_baru, '9':id})
+                            query = text('UPDATE orders \
+                                          SET customer_name=:1, email=:2, phone_number=:3, address=:4, \
+                                          product_name=:5, quantity=:6, order_date=:7, order_status=:8 \
+                                          tracking_number=:9,estimated_arrival_date=:10\
+                                          WHERE id=:11;')
+                            session.execute(query, {'1':customer_name_baru, '2':email_baru, '3':phone_number_baru, '4':address_baru, 
+                                                    '5':product_name_baru, '6':quantity_baru, '7':order_date_baru, '8':order_status_baru, 
+                                                    '9':tracking_number_baru, '10':estimated_arrival_date_baru, '11':id})
                             session.commit()
                             st.experimental_rerun()
                 
                 with col2:
                     if st.form_submit_button('DELETE'):
-                        query = text(f'DELETE FROM schedule WHERE id=:1;')
+                        query = text(f'DELETE FROM orders WHERE id=:1;')
                         session.execute(query, {'1':id})
                         session.commit()
                         st.experimental_rerun()
